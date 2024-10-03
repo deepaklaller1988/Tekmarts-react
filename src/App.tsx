0c9328f5afaf18ad0e1d591d7308a0ea516e0201
dlaller <dlaller1988@gmail.com>
2024-10-03 14:37:23 +0530

    Project setup 03 October 2024

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Header } from "./components/Header";
+import { Home } from "./pages/Home";
+import { Footer } from "./components/Footer";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {/* <Route path="/login" element={} />
+        <Route path="/register" element={} />
+        <Route path="/about" element={} />
+        <Route path="*" element={} /> */}
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
+}
+
+export default App;
